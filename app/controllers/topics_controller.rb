f7ab631d84f6006b0c7250b886ff658da8990546
XavierMeunier <m.xaviermeunier@gmail.com>
2015-06-11 19:10:47 +0200

    [View/Cont] Add topic views & controller

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,13 @@
+class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @topics }
+    end
+  end
+
+
+end
