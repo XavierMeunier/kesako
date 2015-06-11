cf1e1904d2fc5f1d4d22f28a78410203e89b6b13
XavierMeunier <m.xaviermeunier@gmail.com>
2015-06-12 01:45:09 +0200

    [Levels] Add safety for empty levels & add topics with subtopics

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -2,10 +2,10 @@ class LevelsController < ApplicationController
 
   def index
     @levels = Level.all.order(:difficulty)
+    @questions = (@levels.map{|l| l.questions.count} != 0)
 
     respond_to do |format|
       format.html
-      format.json { render json: @levels }
     end
   end
 
@@ -13,10 +13,10 @@ class LevelsController < ApplicationController
     @level = Level.find_by_name(params[:name])
     @question = @level.questions.limit(1).order("RAND()").first
     @answer = Answer.new({question_id: @question.id})
+    @sub_topics = @question.sub_topics
 
     respond_to do |format|
       format.html
-      format.json
     end
   end
 
