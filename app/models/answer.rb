aeed59edd6793d64c2bb1da7fffadc9b5c00774b
XavierMeunier <m.xaviermeunier@gmail.com>
2015-06-12 02:06:36 +0200

    [Validations] Add validation on answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,6 +3,7 @@ class Answer < ActiveRecord::Base
   belongs_to :question
 
   validates :content, presence: true
+  validates :question_id, presence: true
 
   # Set correct if match another good answer
   after_save(on: :create) do 
